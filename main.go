c24602dd4c1f989f269abf355872cba9ebe7826c
Christian Wöhrle <christian.woehrle@googlemail.com>
2017-12-21 13:35:11 +0100

    Set Starttime for StatisticsReport

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/fsouza/go-dockerclient"
-	"regexp"
-	"gopkg.in/alecthomas/kingpin.v2"
-	"time"
-	log "github.com/Sirupsen/logrus"
-	"strings"
-	"sort"
 	"os/signal"
+	"regexp"
+	"sort"
+	"strings"
 	"sync"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/fsouza/go-dockerclient"
+	"gopkg.in/alecthomas/kingpin.v2"
 
 	"os"
+	"strconv"
 )
 
 type container struct {
@@ -37,9 +39,9 @@ var containerDeaths map[string]container
 func main() {
 
 	var (
-		interval = kingpin.Flag("interval", "Statistics every <interval> minutes.").Default("3").Int()
-		//reporttime = kingpin.Flag("reporttime", "Time when report should be printed [hh:mm]").String()
-		logLevel = kingpin.Flag("logLevel", "LogLevel for Program").Default("INFO").Enum("DEBUG", "INFO", "WARNING", "ERROR")
+		interval  = kingpin.Flag("interval", "Statistics every <interval> minutes.").Default("3").Int()
+		starttime = kingpin.Flag("starttime", "Time when report should be printed [hh:mm]").String()
+		logLevel  = kingpin.Flag("logLevel", "LogLevel for Program").Default("INFO").Enum("DEBUG", "INFO", "WARNING", "ERROR")
 	)
 	kingpin.Parse()
 
@@ -48,7 +50,7 @@ func main() {
 
 	//reporttimer := time.Now()
 
-	//fmt.Println(reporttimer, reporttime)
+	//fmt.Println(reporttimer, starttime)
 	switch *logLevel {
 	case "DEBUG":
 		log.SetLevel(log.DebugLevel)
@@ -64,27 +66,40 @@ func main() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	go func() {
-		fmt.Println("start interrupt listener")
+		log.Info("start interrupt listener")
 		for {
 			sig := <-signalChan
 			fmt.Println("\nReceived an interrupt, showstats... :\n", sig)
 			showStatistics()
 		}
 	}()
-	ticker := time.NewTicker(time.Duration(*interval) * time.Minute)
+
+	firstAlert := getFirstAlertTime(*starttime)
+	log.Info("FirstAlert: ", firstAlert)
+	timer := time.NewTimer(time.Until(firstAlert))
+
+	log.Info("TimeUntil: ", time.Until(firstAlert))
+	log.Info("TimeSub: ", firstAlert.Sub(time.Now()))
+
 	go func() {
-		log.Info("Start Statisctics")
-		for {
+		<-timer.C
+		log.Info("Startzeitpunkt erreicht")
+		showStatistics()
+		ticker := time.NewTicker(time.Duration(*interval) * time.Minute)
+		go func() {
+			log.Info("Start Statisctics")
 			for {
-				select {
-				case <-ticker.C:
-					showStatistics()
+				for {
+					select {
+					case <-ticker.C:
+						showStatistics()
+					}
 				}
 			}
-		}
+		}()
+
 	}()
 
-	showStatistics()
 	//pattern für services, containername ist vorne.number.id
 	servicePattern := regexp.MustCompile("\\.([0-9]+)\\.([0-9a-z]+)$")
 
@@ -116,8 +131,7 @@ func main() {
 				dc, err = client.InspectContainer(id)
 				if err != nil {
 					fmt.Println(err)
-				} else
-				{
+				} else {
 					log.Debug("Container died, name:", dc.Name, " Id:", id)
 				}
 			}
@@ -170,5 +184,33 @@ func showStatistics() {
 	for _, k := range cs {
 		fmt.Println(k.deaths, ";", k.name)
 	}
+
+}
+
+func getFirstAlertTime(starttime string) (alarmTime time.Time) {
+
+	hour, _ := strconv.Atoi(string([]rune(starttime)[0:2]))
+	//	fmt.Println(err)
+
+	minute, _ := strconv.Atoi(string([]rune(starttime)[3:]))
+	//	fmt.Println(err)
+
+	//	fmt.Println(string(hour))
+	//fmt.Println(string(minute))
+	now := time.Now()
+	log.Info("Time Now:", now)
+
+	alarmTime = time.Date(now.Year(), now.Month(), now.Day(), hour, minute, 0, 0, now.Location())
+	//fmt.Println(now)
+	//fmt.Println(alarmTime)
+	log.Info("AlarmTime: ", alarmTime)
+	if alarmTime.Before(now) {
+		log.Info("alarmTime before now")
+		alarmTime = alarmTime.Add(24 * time.Hour)
+		//fmt.Println(now)
+
+	}
+	log.Info("AlarmTime: ", alarmTime)
+	return
 
 }
